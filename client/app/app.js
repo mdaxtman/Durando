27db28357e5ce3e208222653c808dba3c4539e89
Mike Axtman <mdaxtman@gmail.com>
2014-09-24 17:35:09 -0700

    add data factory and brand view and controller

--- client/app/app.js
+++ client/app/app.js
@@ -7,6 +7,7 @@ angular.module('retailDashboardApp', [
   'ui.router',
   'ui.bootstrap'
 ])
+
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
       .otherwise('/');
