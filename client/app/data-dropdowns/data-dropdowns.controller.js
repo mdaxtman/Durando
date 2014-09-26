f17bbabdbd5427359da4c1df30421a9b2b9cfb3f
Mike Axtman <mdaxtman@gmail.com>
2014-09-26 16:20:42 -0700

    add drop down menus to the dropdown nav bar

--- client/app/data-dropdowns/data-dropdowns.controller.js
+++ client/app/data-dropdowns/data-dropdowns.controller.js
@@ -2,10 +2,17 @@
 
 angular.module('DropDowns', ['dataModule'])
 
-.controller('DropDownController', function($scope, DataItems, DataBrands, DataCategories) {
+.controller('DropDownController', function($scope, DataItems, DataBrands, DataCategories, DataItemTactics) {
+  $scope.all = 'All';
   $scope.categories = DataCategories.getCategoryData(); 
+  $scope.selectedCategory = $scope.categories;
   $scope.brands = DataBrands.getBrandData();
+  $scope.selectedBrand = $scope.brands;
   $scope.items = DataItems.getItemData();
+  $scope.selectedItem = $scope.items;
+  $scope.tactics = DataItemTactics.getItemTacticData();
+  $scope.selectedTactic = $scope.tactics;
+  
 });
 
 
