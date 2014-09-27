4f20894a1004f07c58b5f8afafff2ad962ab3753
Mike Axtman <mdaxtman@gmail.com>
2014-09-26 22:11:56 -0700

    disabled inactive dropdowns, filter results of dropdowns base on previous selections

--- client/app/data-dropdowns/data-dropdowns.controller.js
+++ client/app/data-dropdowns/data-dropdowns.controller.js
@@ -5,15 +5,67 @@ angular.module('DropDowns', ['dataModule'])
 .controller('DropDownController', function($scope, DataItems, DataBrands, DataCategories, DataItemTactics) {
   $scope.all = 'All';
   $scope.categories = DataCategories.getCategoryData(); 
-  $scope.selectedCategory = $scope.categories;
+  $scope.selectedCategory = null;
   $scope.brands = DataBrands.getBrandData();
-  $scope.selectedBrand = $scope.brands;
+  $scope.selectedBrand = null;
   $scope.items = DataItems.getItemData();
-  $scope.selectedItem = $scope.items;
+  $scope.selectedItem = null;
   $scope.tactics = DataItemTactics.getItemTacticData();
-  $scope.selectedTactic = $scope.tactics;
-  
-});
+  $scope.selectedTactic = null;
+  $scope.selection = function(selectedItem) {
+    if($scope.selectedCategory === null){
+      $scope.selectedBrand = null;
+    }
+    if($scope.selectedBrand === null){
+      $scope.selectedItem = null;
+    }
+    if($scope.selectedItem === null){
+      $scope.selectedTactic = null;
+    }
+  };
+})
+
+.filter('brandFilter', function(){
+  return function(items, selectedCategory){
+    if(selectedCategory !== null){
+      var results = items.filter(function(val){
+        if(val.Category === selectedCategory.id){
+        return val;
+        }
+      });
+      return results;
+    }
+  };
+})
+
+.filter('itemFilter', function(){
+  return function(items, selectedBrand){
+    if(selectedBrand !== null){
+      var results = items.filter(function(val){
+        if(val.brand === selectedBrand.id){
+        return val;
+        }
+      });
+      return results;
+    }
+  };
+})
+
+.filter('tacticFilter', function(){
+  return function(items, selectedItem){
+    if(selectedItem !== null){
+      var results = items.filter(function(val){
+        console.log(selectedItem);
+        console.log(val);
+        if(val.item === selectedItem.item){
+        return val;
+        }
+      });
+      return results;
+    }
+  };
+})
+
 
 
 
